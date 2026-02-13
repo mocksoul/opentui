b1bcfdee8f85c356ed492d64b22b525af47d28e7
Simon Klee <hello@simonklee.dk>
2026-02-13 16:31:02 +0100

    fix Bun imports

--- packages/core/src/renderables/LineNumberRenderable.ts
+++ packages/core/src/renderables/LineNumberRenderable.ts
@@ -2,6 +2,7 @@ import { Renderable, type RenderableOptions } from "../Renderable"
 import { OptimizedBuffer } from "../buffer"
 import type { RenderContext, LineInfoProvider } from "../types"
 import { RGBA, parseColor } from "../lib/RGBA"
+import { stringWidth } from "../runtime"
 import { MeasureMode } from "yoga-layout"
 
 export interface LineSign {
@@ -155,11 +156,11 @@ class GutterRenderable extends Renderable {
 
     for (const sign of this._lineSigns.values()) {
       if (sign.before) {
-        const width = Bun.stringWidth(sign.before)
+        const width = stringWidth(sign.before)
         this._maxBeforeWidth = Math.max(this._maxBeforeWidth, width)
       }
       if (sign.after) {
-        const width = Bun.stringWidth(sign.after)
+        const width = stringWidth(sign.after)
         this._maxAfterWidth = Math.max(this._maxAfterWidth, width)
       }
     }
@@ -277,7 +278,7 @@ class GutterRenderable extends Renderable {
         // Draw 'before' sign if present
         const sign = this._lineSigns.get(logicalLine)
         if (sign?.before) {
-          const beforeWidth = Bun.stringWidth(sign.before)
+          const beforeWidth = stringWidth(sign.before)
           // Pad to max before width for alignment
           const padding = this._maxBeforeWidth - beforeWidth
           currentX += padding
